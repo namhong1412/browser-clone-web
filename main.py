7b50bde24d62db372663ee8e0a7b852a9b843c59
Hong Nguyen Nam <kaitokid1412.conan@gmail.com>
2022-12-07 21:58:41 +0700

    🎉 Remove chromedriver, bypass requests blocking sites

--- main.py
+++ main.py
@@ -3,17 +3,14 @@
 # __copyright__ = "Copyright 2022, The Browser Clone"
 # __license__ = "GPL"
 # __version__ = "2.1.0"
-# __maintainer__ = "Hong Nguyen Nam (Jeremy Nguyen)"
 # __email__ = "a2FpdG9raWQxNDEyLmNvbmFuQGdtYWlsLmNvbQ=="
-__path_driver__ = '/Users/Hacker1945/Desktop/clone-web/chromedriver'
 __black_list_type__ = ['.php']
 __status_code__ = [200, 404]
 __clone_all__ = False
-__zip__ = True
+__zip__ = False
+__headless__ = False
 __clone_url__ = 'https://themesbrand.com/velzon/html/default/index.html'
 
-# https://unistudio.co/html/renox/main/index.html
-# https://themesbrand.com/velzon/html/default/index.html
 import os
 import os.path
 import re
@@ -25,7 +22,47 @@ import requests
 from bs4 import BeautifulSoup
 from seleniumwire import webdriver
 from tqdm import tqdm
+from webdriver_manager.chrome import ChromeDriverManager
 from zipfile36 import ZipFile
+
+
+def extract_info_url(url, main=False):
+    data_url = urlparse(url)
+    domain = data_url.netloc
+    path_file = domain.replace('.', '') + os.path.split(data_url.path)[0] + '/'
+    file_name = os.path.split(data_url.path)[1]
+    scheme = data_url.scheme
+    url_ori = url.replace(file_name, '')
+    black_list = ['', '/']
+    if main == True and file_name in black_list:
+        file_name = 'index.html'
+    return {"domain": domain, "path": path_file, "file_name": file_name, "scheme": scheme, "url": url_ori}
+
+
+def get_all_file_paths(directory):
+    file_paths = []
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)
+    return file_paths
+
+
+def compress(path_folder):
+    print(f'Compression files... {str(path_folder)}.zip')
+    directory = path_folder
+    file_paths = get_all_file_paths(directory)
+    with ZipFile(f'{path_folder}.zip', 'w') as zip:
+        for file in file_paths:
+            zip.write(file)
+    print('All files zipped successfully!')
+
+
+def check_invalid(file_name):
+    regex = r"[a-z-0-9]+.html"
+    matches = re.finditer(regex, file_name, re.MULTILINE)
+    for match in matches:
+        return match.group()
 
 
 class File:
@@ -33,25 +70,13 @@ class File:
 
     def __init__(self, url):
         self.url = url
-        self.info_url = self.extract_info_url(url, True)
+        self.info_url = extract_info_url(url, True)
         self.check_exists(url)
 
-    def extract_info_url(self, url, main=False):
-        data_url = urlparse(url)
-        domain = data_url.netloc
-        path_file = domain.replace('.', '') + os.path.split(data_url.path)[0] + '/'
-        file_name = os.path.split(data_url.path)[1]
-        scheme = data_url.scheme
-        url_ori = url.replace(file_name, '')
-        black_list = ['', '/']
-        if main == True and file_name in black_list:
-            file_name = 'index.html'
-        return {"domain": domain, "path": path_file, "file_name": file_name, "scheme": scheme, "url": url_ori}
-
-    def download_file(self, url):
-        info_url = self.extract_info_url(url)
+    def download_file(self, url, headers):
+        info_url = extract_info_url(url)
         if url == self.url:
-            info_url = self.extract_info_url(url, True)
+            info_url = extract_info_url(url, True)
 
         if info_url['file_name'][-4:] not in __black_list_type__:
             file_name = info_url['file_name']
@@ -60,38 +85,31 @@ class File:
                 file_name = 'index.html'
             path_file = info_url['path'] + file_name
             if not os.path.exists(path_file):
-                r = requests.get(url)
+                r = requests.get(url, headers=headers)
                 os.makedirs(os.path.dirname(path_file), exist_ok=True)
                 with open(path_file, 'wb') as f:
                     f.write(r.content)
 
-    def check_invalid(self, file_name):
-        regex = r"[a-z-0-9]+.html"
-        matches = re.finditer(regex, file_name, re.MULTILINE)
-        for match in matches:
-            return match.group()
-
     def check_exists(self, url):
-        info_url = self.extract_info_url(url)
-        path_file = info_url['path'] + info_url['file_name']
-        if info_url['domain'] == self.info_url['domain']:
-            return os.path.exists(path_file) == False
-        else:
+        info_url = extract_info_url(url)
+        if info_url['domain'] != self.info_url['domain']:
             return False
+        path_file = info_url['path'] + info_url['file_name']
+        return os.path.exists(path_file) == False
 
-    def get_href_a_tag(self, page_source):
+    def get_all_urls_in_page(self, page_source):
         result = []
         source = BeautifulSoup(page_source, 'html.parser')
         try:
             data_a = source.find_all("a")
         except Exception:
             data_a = None
-        a_tag_list = []
+        a_tag = []
         for a in data_a:
-            if a.get('href') != '' and a.get('href') != '#' and str(a.get('href')) not in a_tag_list and self.check_invalid(str(a.get('href'))) is not None:
-                a_tag_list.append(a.get('href'))
+            if a.get('href') != '' and a.get('href') != '#' and str(a.get('href')) not in a_tag and check_invalid(str(a.get('href'))) is not None:
+                a_tag.append(a.get('href'))
 
-        for href in a_tag_list:
+        for href in a_tag:
             domain = urlparse(href).netloc
             if domain == '':
                 if len(href.split('../')) > 1:
@@ -111,29 +129,13 @@ class File:
                 result.append(href)
         return result
 
-    def get_all_file_paths(self, directory):
-        file_paths = []
-        for root, directories, files in os.walk(directory):
-            for filename in files:
-                filepath = os.path.join(root, filename)
-                file_paths.append(filepath)
-        return file_paths
-
-    def zip(self, path_folder):
-        print(f'Compression files... {str(path_folder)}.zip')
-        directory = path_folder
-        file_paths = self.get_all_file_paths(directory)
-        with ZipFile(f'{path_folder}.zip', 'w') as zip:
-            for file in file_paths:
-                zip.write(file)
-        print('All files zipped successfully!')
-
 
 class BrowserClone(File):
     driver = ''
     page_source = ''
-    all_tab = []
+    all_url = []
     url_down = []
+    headers = {}
 
     def __init__(self, url):
         super().__init__(url)
@@ -143,54 +145,40 @@ class BrowserClone(File):
     def open_browser(self):
         print('============================== Begin ==============================')
         options = webdriver.ChromeOptions()
-        options.add_argument("--incognito")
-        options.add_argument("--headless")
-        # options.add_argument(f"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36")
-        # Configure to run in the background on ubuntu/debian
+        if __headless__:
+            options.add_argument('--headless')
         options.add_argument("--no-sandbox")
-        # Bypass network to fast loading
-        # options.add_argument('--no-proxy-server')
-        # options.add_argument("--proxy-server='direct://'")
-        # options.add_argument("--proxy-bypass-list=*")
-
         options.add_experimental_option("useAutomationExtension", False)
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
         options.page_load_strategy = 'none'
 
-        path_chrome_driver = __path_driver__
-        self.driver = webdriver.Chrome(chrome_options=options, executable_path=path_chrome_driver)
+        self.driver = webdriver.Chrome(chrome_options=options, executable_path=ChromeDriverManager().install())
         self.driver.get(self.url)
+        print('Waiting 30s to make sure the page has finished loading...')
         time.sleep(30)
-        for _ in range(5):
-            try:
-                self.driver.switch_to.alert.accept()
-            except Exception:
-                continue
-        self.page_source = self.driver.page_source
-        super().__init__(self.url)
+        self.set_page_source()
         self.extract_file()
+
         print('Getting all the links to crawl...')
-        url_tab_data = super().get_href_a_tag(self.page_source)
-        for url_tab in url_tab_data:
-            self.all_tab.append(url_tab)
-            self.extract_html(url_tab)
+        all_urls_in_page = super().get_all_urls_in_page(self.page_source)
+        for url_in_page in all_urls_in_page:
+            self.all_url.append(url_in_page)
+            self.extract_html(url_in_page)
 
         # clone options
         if __clone_all__:
-            data = list(set(self.all_tab))
-            for url in data:
+            all_url = list(set(self.all_url))
+            for url in all_url:
                 self.driver.get(url)
                 self.extract_file()
 
-        print('Get all link clone done!')
-        print('Save files...')
+        print('Get all the links done!')
         self.extract_file(True)
-        print('Save files Done!')
 
         if __zip__:
-            url_info = super().extract_info_url(self.url, True)
+            url_info = extract_info_url(self.url, True)
             folder = './' + url_info['domain'].replace('.', '')
-            super().zip(folder)
+            compress(folder)
             try:
                 shutil.rmtree(folder, ignore_errors=True)
             except OSError as e:
@@ -200,15 +188,11 @@ class BrowserClone(File):
     def extract_html(self, url):
         super().__init__(url)
         self.driver.get(url)
-        for _ in range(5):
-            try:
-                self.driver.switch_to.alert.accept()
-            except Exception:
-                continue
-        self.page_source = self.driver.page_source
-        url_tab_data = super().get_href_a_tag(self.page_source)
-        for url_tab in url_tab_data:
-            self.all_tab.append(url_tab)
+        self.set_page_source()
+        all_urls_in_page = super().get_all_urls_in_page(self.page_source)
+        for url_in_page in all_urls_in_page:
+            self.all_url.append(url_in_page)
+            self.headers = self.driver.requests[0].headers
 
     def extract_file(self, down=False):
         for request in self.driver.requests:
@@ -219,13 +203,23 @@ class BrowserClone(File):
             ):
                 self.url_down.append(request.url)
         if down:
+            print('Save files...')
             super().__init__(self.url)
             data = list(set(self.url_down))
             with tqdm(total=len(data)) as pbar:
                 for file in data:
                     if super().check_exists(file):
-                        super().download_file(file)
+                        super().download_file(file, self.headers)
                     pbar.update(1)
+            print('Save files Done!')
+
+    def set_page_source(self):
+        for _ in range(5):
+            try:
+                self.driver.switch_to.alert.accept()
+            except Exception:
+                continue
+        self.page_source = self.driver.page_source
 
 
 BrowserClone(__clone_url__)
